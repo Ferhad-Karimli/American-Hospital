d070a4f1cb2686268deef3f2dc9ad0832ba52592
Ferhad-Karimli <ferhad.karimli.077@gmail.comm>
2022-05-31 16:16:27 +0400

    Done

---
+++ american-hospital/Components/footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from "./footer.module.css"
+
+
+export default function footer() {
+  return (
+    <footer className={style.container}>
+
+      <div className={style.contact} >
+          <h2>Contacts</h2>
+
+      </div>
+      
+    </footer>
+  )
+}
